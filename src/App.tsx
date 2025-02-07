12cf8b51d8a821fb8175a4a99714db549adccac2
Vinaykumar <gunjivinaykumar2001@gmail.com>
2025-02-07 13:15:03 +0530

    changed the resume

--- src/App.tsx
+++ src/App.tsx
@@ -159,7 +159,7 @@ function App() {
               <a href="#projects" className="border border-white hover:bg-white hover:text-gray-900 px-8 py-3 rounded-full transition-all transform hover:scale-105 animate-pulse">
                 View Work
               </a>
-              <a href="/Resume.pdf" download={"Vinaykumar@Resume"} className="border border-white hover:bg-white hover:text-gray-900 px-8 py-3 rounded-full transition-all transform hover:scale-105 animate-pulse">
+              <a href="../components/Resume.pdf" download={"Vinaykumar@Resume"} className="border border-white hover:bg-white hover:text-gray-900 px-8 py-3 rounded-full transition-all transform hover:scale-105 animate-pulse">
                 Check Resume
               </a>
             </div>
